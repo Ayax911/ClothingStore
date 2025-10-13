b3a338ff3deef5b3032f09b5f84365fcbe1a6685
Ayax911 <j.mast8924@gmail.com>
2025-10-12 21:07:36 -0500

    Modificacion DB y appsettings.json

--- src/ClothingStore.Api/obj/Debug/net9.0/ClothingStore.Api.AssemblyInfo.cs
+++ src/ClothingStore.Api/obj/Debug/net9.0/ClothingStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff867158d5adea19785a08d0635480a53572a770")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91db7d2d830ef07004b6e57738d81ea331a7eab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
